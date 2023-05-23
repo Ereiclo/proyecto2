395b3a68a8551673bb52dab09c056e6f308a0511
Eric <eric.bracamonte@utec.edu.pe>
2023-05-23 18:31:51 -0500

    fixed another dbscan bug

--- dbscan.py
+++ dbscan.py
@@ -7,13 +7,16 @@ from tqdm import tqdm
 def hasLabel(label):
     return label >= 0
 
+def isNoise(label):
+    return label == 0
+
 X, y = load_iris(return_X_y=True)
 
-# X = np.unique(X, axis=0)
 X = np.unique(X.T[2:3+1].T, axis=0)
 tree = KDTree(X)
 min_vecinos = 3 
-r = 0.6
+r = 0.5
+eps = .0000000001
 
 
 labels = np.full(shape=X.shape[0], fill_value=-1)
@@ -24,21 +27,7 @@ for p_i in range(len(X)):
     if hasLabel(labels[p_i]):
         continue
 
-    # indexes = tree.query_radius([X[p_i]],r=r)[0]
-    # print(X[p_i].reshape(1,-1).shape)
-    [indexes],[_] = tree.query_radius([X[p_i]],r=r,return_distance=True,sort_results=True)
-    # print(p_i)
-    # print(indexes)
-    # print(_)
-
-    # print(p_i)
-    # print(tree.query_radius([X[p_i]],r=r,return_distance=True,sort_results=True))
-    # break
-    # if p_i < 10:
-    #     continue
-    # else:
-    #     break
-
+    [indexes],[_] = tree.query_radius([X[p_i]],r=r + eps,return_distance=True,sort_results=True)
 
     if len(indexes) < min_vecinos:
         labels[p_i] = 0 
@@ -51,33 +40,50 @@ for p_i in range(len(X)):
     i = 0
     while i < len(indexes):
         p_v = indexes[i]
-        print(indexes,p_v)
 
-        if labels[p_v] == 0:
+        if isNoise(labels[p_v]):
             labels[p_v] = actual_label 
 
-        if labels[p_v] > 0:
+        if hasLabel(labels[p_v]) :
+            # print('pipipi',labels[p_v],p_v)
             i += 1
             continue
 
         labels[p_v] = actual_label 
 
-        new_indexes = tree.query_radius([X[p_v]],r=r)[0]
+        [new_indexes],[_] = tree.query_radius([X[p_v]],r=r+ eps,return_distance=True,sort_results=True)
+        # [new_indexes2],[_] = tree.query_radius([X[p_v]],r=r+.0000000001,return_distance=True,sort_results=True)
+
+        # print(X[p_v])
+        # print(X[new_indexes])
+        # print(new_indexes)
+        # print(_)
 
         if len(new_indexes) < min_vecinos:
             i += 1
             continue
+
+
+        # print(p_v)
+       
+        # print(_)
+
         
-        indexes = np.concatenate([indexes, new_indexes])
-
+        # print(p_v)
+        # print(new_indexes)
+        # print(_)
+        indexes = np.concatenate([indexes, new_indexes[1:]])
+        # print(indexes)
         i += 1
-    
-    break
 
+    # print({l for l in labels})
+    # plt.scatter(X.T[0], X.T[1], c=labels+1)
+    # plt.show()
 
     actual_label += 1
-    
 
-# plt.scatter(X.T[0], X.T[1], c=labels)
-# plt.show()
+
+# print({l for l in labels})
+plt.scatter(X.T[0], X.T[1], c=labels+1)
+plt.show()
 
