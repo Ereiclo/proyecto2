d694748e11d41703e48f5df86b1e6d2f3ad3ee88
Eric <eric.bracamonte@utec.edu.pe>
2023-05-23 16:57:20 -0500

    refactor dbscan

--- dbscan.py
+++ dbscan.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
+def hasLabel(label):
+    return label >= 0
+
 X, y = load_iris(return_X_y=True)
 
+# X = np.unique(X, axis=0)
 X = np.unique(X.T[2:3+1].T, axis=0)
 tree = KDTree(X)
 min_vecinos = 3 
-r = 0.4
+r = 0.6
 
 
 labels = np.full(shape=X.shape[0], fill_value=-1)
@@ -17,22 +21,37 @@ labels = np.full(shape=X.shape[0], fill_value=-1)
 actual_label = 1
 
 for p_i in range(len(X)):
-    if labels[p_i] >= 0:
+    if hasLabel(labels[p_i]):
         continue
-    indexes = tree.query_radius([X[p_i]],r=r)[0]
 
-    if len(X[indexes]) < min_vecinos:
+    # indexes = tree.query_radius([X[p_i]],r=r)[0]
+    # print(X[p_i].reshape(1,-1).shape)
+    [indexes],[_] = tree.query_radius([X[p_i]],r=r,return_distance=True,sort_results=True)
+    # print(p_i)
+    # print(indexes)
+    # print(_)
+
+    # print(p_i)
+    # print(tree.query_radius([X[p_i]],r=r,return_distance=True,sort_results=True))
+    # break
+    # if p_i < 10:
+    #     continue
+    # else:
+    #     break
+
+
+    if len(indexes) < min_vecinos:
         labels[p_i] = 0 
         continue
 
     labels[p_i] = actual_label
-
 
     indexes = indexes[1:]
 
     i = 0
     while i < len(indexes):
         p_v = indexes[i]
+        print(indexes,p_v)
 
         if labels[p_v] == 0:
             labels[p_v] = actual_label 
@@ -52,11 +71,13 @@ for p_i in range(len(X)):
         indexes = np.concatenate([indexes, new_indexes])
 
         i += 1
+    
+    break
 
 
     actual_label += 1
     
 
-plt.scatter(X.T[0], X.T[1], c=labels)
-plt.show()
+# plt.scatter(X.T[0], X.T[1], c=labels)
+# plt.show()
 
